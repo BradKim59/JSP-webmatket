f56e94623862cb7480eb481d5668d5380e0e001a
BradKim59 <63415138+BradKim59@users.noreply.github.com>
2022-05-20 15:17:21 +0900

    git test

--- src/main/java/dao/ProductRepository.java
+++ src/main/java/dao/ProductRepository.java
@@ -1,37 +1,51 @@
 package dao;
+//data access object
 
 import java.util.ArrayList;
 import java.util.List;
 
 import dto.Product;
 
-//data access object
 
 // product에 대한 기능을 정의
 public class ProductRepository {
 	// ArrayList는 클래스
 	// List는 인터페이스, 생성시 어떠한 리스트 종류도 받아들일 수 있다.
 	private List<Product> products = new ArrayList<>();
+	
+	// 하나의 객체를 생성 한 후 인스턴스를 재사용하는 싱글턴 패턴
+	private static ProductRepository instance = new ProductRepository();
+	
+	public static ProductRepository getInstance() {
+		
+		return instance;
+	}
+	
+	
+	
+	
 	//원래는 DB에서 가져온다
 	public ProductRepository() {
 		Product phone = new Product("P1234", "iphone 6s", 800000);
-		phone.setDescription("4.7-inch, 1334X750 Retina HD display, 어쩌구");
+		phone.setDescription("4.7-inch, 1334X750 Retina HD display, 8-megapixel iSight Camera");
 		phone.setCategory("smartPhone");
 		phone.setMenufacturer("Apple");
-		phone.getUnitsInStock(1000);
+		phone.setUnitsInStock(1000);
 		phone.setCategory("new");
+		
 		Product notebook = new Product ("P1235", "LG PC 그램", 1500000);
-		notebook.setDescription("4.7-inch, 1334X750 Retina HD display, 어쩌구");
-		notebook.setCategory("smartPhone");
-		notebook.setMenufacturer("Apple");
-		notebook.getUnitsInStock(1000);
-		notebook.setCategory("new");
-		Product tablet = new Product ("P1236", "Galaxy", 900000);
-		tablet.setDescription("4.7-inch, 1334X750 Retina HD display, 어쩌구");
-		tablet.setCategory("smartPhone");
-		tablet.setMenufacturer("Apple");
-		tablet.getUnitsInStock(1000);
-		tablet.setCategory("new");
+		notebook.setDescription("13.3-inch, IPS LED display, 5rd Generation Intel core processors");
+		notebook.setCategory("Notebook");
+		notebook.setMenufacturer("LG");
+		notebook.setUnitsInStock(1000);
+		notebook.setCategory("Refurbished");
+		
+		Product tablet = new Product ("P1236", "Galaxy Tab S", 900000);
+		tablet.setDescription("212.8*125.6*6.6mm, Super AMOLED display, Octa-Core processor");
+		tablet.setCategory("Tablet");
+		tablet.setMenufacturer("Samsung");
+		tablet.setUnitsInStock(1000);
+		tablet.setCategory("Old");
 		
 		products.add(phone);
 		products.add(notebook);
@@ -42,4 +56,17 @@ public class ProductRepository {
 		return products;
 	}
 	
+	// ID로상품찾기
+	public Product getProductById(String productId) {
+		// 자바 8 이후 사용가능한 문법, stream. 원래코드는 p.173 참고
+		return products
+				.stream() // 상품 3개가 흘러감
+				.filter((product)-> product.getProductId().equals(productId))// 필요한것 거를 규칙 설정 (true인것만 찾기)
+				.findFirst() // 거른것 중 첫번째 것
+				.get(); // 얻어오기
+	}
+	//상품 추가
+	public void addProduct(Product product) {
+		products.add(product);
+	}
 }
--- src/main/java/dto/Product.java
+++ src/main/java/dto/Product.java
@@ -3,10 +3,12 @@ package dto;
 import java.io.Serializable;
 
 // 모델 클래스
-// JSP : 자바빈즈(getter/setter, 기본 생성자, serializable)
+// JSP : 자바빈즈(getter/setter, 기본 생성자, serializable) 일단 무시
+// getter setter만 있으면 자바빈즈로 보낸다.
 public class Product implements Serializable{
 
 	// 자바 직렬화에 사용, 
+	// 이 상태는 객체를 각기 생성하는것이 아니므로, 없어도 된다.
 	private static final long serialVersionUID = 3618599846602713926L;
 	
 	private String productId;
@@ -15,12 +17,13 @@ public class Product implements Serializable{
 	private String description;
 	private String menufacturer;
 	private String category;
-	private long unitsInStock;
+	private int unitsInStock;
 	private String conidition;
 	
 	//기본 생성자로 생성자 있는것처럼 빈자바 룰에 맞춰줌
 	public Product() {};
 	
+	// javabeans 에서 action Tag 사용 시 parameter로 사용 예정
 	public Product(String productId, String name, int unitPrice) {
 		this.productId = productId;
 		this.name = name;
@@ -62,10 +65,10 @@ public class Product implements Serializable{
 	public void setCategory(String category) {
 		this.category = category;
 	}
-	public long getUnitsInStock(int i) {
+	public int getUnitsInStock() {
 		return unitsInStock;
 	}
-	public void setUnitsInStock(long unitsInStock) {
+	public void setUnitsInStock(int unitsInStock) {
 		this.unitsInStock = unitsInStock;
 	}
 	public String getConidition() {
@@ -76,6 +79,7 @@ public class Product implements Serializable{
 	}
 
 	@Override
+	// toString이 없다면 각 객체의 해쉬코드, 메모리 주소값이 나온다.(실제 주소는 아님)
 	public String toString() {
 		return "Product [productId=" + productId + ", name=" + name + ", unitPrice=" + unitPrice + ", description="
 				+ description + ", menufacturer=" + menufacturer + ", category=" + category + ", unitsInStock="
